96fa8cb5a603b8fb461baa5db9cbb3c00b2b5c2a
hasanmh9988770-MHR <hasanmh9988770@gmail.com>
2026-02-04 16:17:31 +0600

    Update conversation operations.js

--- 01 basics/conversation operations.js
+++ 01 basics/conversation operations.js
@@ -1,4 +1,4 @@
-let score = "hitesh"
+let score = "Mehedy"
 
 //console.log(typeof score);
 //console.log(typeof(score));
@@ -12,7 +12,7 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "Mehedy"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
