cca2c798f6a25cd18e6efafae24f8224b5409abc
hasanmh9988770-MHR <hasanmh9988770@gmail.com>
2026-02-14 12:42:27 +0600

    Update string values in 05_strings.js

--- 01 basics/05_strings.js
+++ 01 basics/05_strings.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
+const name = "mehedi"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('mehedy-mh-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -22,14 +22,14 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   mehedi    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://mehedi.com/mehedi%20hasan"
 
 console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+console.log(url.includes('rabby'))
 
 console.log(gameName.split('-'));
